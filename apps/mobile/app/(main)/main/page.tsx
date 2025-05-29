da3d8951a7807c1ed1cc47f77cb22c1cf0a6f7e7
unknown <corosica1@gmail.com>
2025-05-29 15:49:32 +0900

    Feat : 검색 창 퍼블리싱

--- apps/mobile/app/(main)/main/page.tsx
+++ apps/mobile/app/(main)/main/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AlertButton from "@/components/common/AlertButton";
 import ItemCard from "@/components/common/ItemCard";
+import Search from "@/components/common/Search";
 export default function page() {
   return (
     <div>
-      <ItemCard />
+      <Search />
       <AlertButton isActive={true} />
+      <ItemCard />
     </div>
   );
 }
