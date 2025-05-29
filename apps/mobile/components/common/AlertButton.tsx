5d23a6c468a76704bf426a38c4e05c4e79dec072
unknown <corosica1@gmail.com>
2025-05-29 15:49:17 +0900

    Feat : 알람 버튼 크기 조정

--- apps/mobile/components/common/AlertButton.tsx
+++ apps/mobile/components/common/AlertButton.tsx
@@ -7,7 +7,7 @@ export default function AlertButton({
   isActive?: boolean;
 }) {
   return (
-    <div className="relative w-14 h-14 flex items-center justify-center bg-custom-green rounded-full">
+    <div className="relative w-12 h-12 flex items-center justify-center bg-custom-green rounded-full">
       <AlertIcon />
       {isActive && (
         <span className="absolute top-0 right-[3px] w-3 h-3 bg-custom-red rounded-full border-2 border-background" />
