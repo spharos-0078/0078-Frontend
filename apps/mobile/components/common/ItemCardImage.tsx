67774fcb3807de6347187125ccc6f54e2abe6acb
unknown <corosica1@gmail.com>
2025-05-30 16:22:06 +0900

    Feat : 이미지 컴포넌트 수정

--- apps/mobile/components/common/ItemCardImage.tsx
+++ apps/mobile/components/common/ItemCardImage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Image from "next/image";
 
-export default function ItemCardImage() {
+export default function ItemCardImage({
+  remainingTime,
+  thumbnail,
+}: {
+  remainingTime: string;
+  thumbnail: string;
+}) {
   return (
     <div className="relative w-full h-40 rounded-t-xl">
       <Image
-        src="https://previews.123rf.com/images/adrian1991/adrian19912109/adrian1991210900001/174101000-%EB%B3%84%EC%9D%B4-%EB%B9%9B%EB%82%98%EB%8A%94-%EB%B0%A4-%EB%B9%88%EC%84%BC%ED%8A%B8-%EB%B0%98-%EA%B3%A0%ED%9D%90-%EA%B7%B8%EB%A6%BC.jpg"
+        src={thumbnail}
         alt="artwork"
         fill={true}
         objectFit="cover"
@@ -15,7 +21,7 @@ export default function ItemCardImage() {
         <p className="text-center">
           남은 시간
           <br />
-          18h : 32m : 04s
+          {remainingTime}
         </p>
       </div>
       <div className="absolute bottom-3 left-4 bg-custom-red text-white text-xs px-2 py-0.5 rounded shadow">
